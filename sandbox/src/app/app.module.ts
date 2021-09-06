01cb1686ae3e76bf696effa3d332b3d1732ff265
Engin Karataş <43602725+EnginKARATAS@users.noreply.github.com>
2021-09-06 19:28:00 +0300

    .

--- sandbox/src/app/app.module.ts
+++ sandbox/src/app/app.module.ts
@@ -3,27 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { WelcomeComponent } from './home/welcome/welcome.component';
-import { ProductModule } from './products/product.module';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-   
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    
     RouterModule.forRoot([
-      {path:'welcome', component: WelcomeComponent},
-      {path:'', component: WelcomeComponent}
+      // {path:'', component: }
     ]),
-    ProductModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
