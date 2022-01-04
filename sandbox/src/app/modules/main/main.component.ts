3f091b7a7eb20f332daec8f1d04544de9cd204f3
engin.karatas@dvu.com.tr <43602725+EnginKARATAS@users.noreply.github.com>
2022-01-05 00:03:28 +0300

    update main.component.ts

--- sandbox/src/app/modules/main/main.component.ts
+++ sandbox/src/app/modules/main/main.component.ts
@@ -8,7 +8,7 @@ import { Switch } from './switch';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
+//sandbox
   switches?: Switch[] = [];
 
   constructor(private spService:SwitchPortService) {
